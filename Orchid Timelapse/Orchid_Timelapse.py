d8bfdc818cfa3ea38e1b79067930306ea3a6cb9a
Steven Cohen <peragwin@gmail.com>
2014-03-23 01:35:50 -0700

    progress display

--- Orchid Timelapse/Orchid_Timelapse.py
+++ Orchid Timelapse/Orchid_Timelapse.py
@@ -8,26 +8,24 @@ _directory = "C:\\Users\\Steven\\Google Drive\\Orchid Timelapse\\"
 _outputfilename = "orchid.avi" #"C:\\Users\\Steven\\Desktop\\orchidvideo.avi"
 
 
-images = glob.iglob(_directory+"B*.png")
-
+images = glob.glob(_directory+"A*.png")
+num_images = len(images)
 #cv2.namedWindow("Orchid Timelapse")
 
-image_array = []
-
-for im in images:
-    orchid = cv2.imread(im)
-    image_array += [orchid]
-    #cv2.imshow("Orchid Timelapse", orchid)
-    #waitKey(100)
-
-num_images = len(image_array)
-
-height, width, layers = image_array[0].shape
+im1 = images[0]
+img1 = cv2.imread(im1)
+height, width, layers = img1.shape
 
 video = cv2.VideoWriter(_outputfilename,-1,12,(width,height),isColor=True)
+
 if video.isOpened():
-    for i in range(num_images):
-        video.write(image_array[i])
+    video.write(img1)
+    n = 1
+    for img_path in images:
+        img = cv2.imread(img_path)
+        video.write(img)
+        print("%.2f processed."%(100*n/float(num_images)))
+        n+=1
 else:
     print "error creating video file", _outputfilename
 
